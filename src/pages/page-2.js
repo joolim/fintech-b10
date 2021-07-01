67cd877b604fc47103ba141c1790591daf29ac64
joolim <joolim625@gmail.com>
2021-07-01 18:46:02 +0800

    reset image

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -8,16 +8,7 @@ const SecondPage = () => (
   <Layout>
     <Seo title="PC2 Cleared!" />
     <h1>On we go to PC3 and the unknown.</h1>
-    <p>Our future evolution
-    <StaticImage
-      src="../images/robo-evolution.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="robo-evolution"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    </p>
+    <p>Our future evolution</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
